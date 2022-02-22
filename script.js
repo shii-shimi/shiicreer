dc529a74784dcc33862027d2427b79dce9e1117a
shii-shimi <you@example.com>
2022-02-22 14:03:04 +0900

    追加

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(function () {
     const fade_bottom = 50; // 画面下からどの位置でフェードさせるか(px)
     const fade_move = 100; // どのぐらい要素を動かすか(px)
-    const fade_time = 1000; // フェードの時間(ms)
+    const fade_time = 1500; // フェードの時間(ms)
     // フェード前のcssを定義
     $(".profile").css({
       opacity: 0,
@@ -70,4 +70,48 @@ $(function () {
     });
 });
 
+
+$(function () {
+  const fade_bottom = 50; // 画面下からどの位置でフェードさせるか(px)
+  const fade_move = 100; // どのぐらい要素を動かすか(px)
+  const fade_time = 1500; // フェードの時間(ms)
+  // フェード前のcssを定義
+  $(".consept").css({
+    opacity: 0,
+    transform: "translateY(" + fade_move + "px)",
+    transition: fade_time + "ms",
+  });
+  // スクロールまたはロードするたびに実行
+  $(window).on("scroll load", function () {
+    const scroll_top = $(this).scrollTop();
+    const scroll_bottom = scroll_top + $(this).height();
+    const fade_position = scroll_bottom - fade_bottom;
+    $(".consept").each(function () {
+      const this_position = $(this).offset().top;
+      if (fade_position > this_position) {
+        $(this).css({
+          opacity: 1,
+          transform: "translateY(0)",
+        });
+      }
+    });
+  });
+});
+
+
+
+
+
+$(".gallery").modaal({
+  type:'image',
+  overlay_close:true,//モーダル背景クリック時に閉じるか
+  before_open:function(){//モーダルが開く前に行う動作
+    $('html').css('overflow-y','hidden');/*縦スクロールバーをださない*/
+    },
+    after_close:function(){//モーダル閉じた後に行う動作
+      $('html').css('overflow-y','scroll');/*縦スクロールバー出す */
+    }
+
+   /* gallery_cacel_button_text:'×',*/
+});
 
